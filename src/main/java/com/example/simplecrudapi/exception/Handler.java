36ad83dbcde6d3c7d0e8681ac2da7a58ff0d51a0
Diana Trojak <diana.trojak@intive.com>
2025-02-03 10:43:24 +0100

    first commit

--- src/main/java/com/example/simplecrudapi/exception/Handler.java
+++ src/main/java/com/example/simplecrudapi/exception/Handler.java
@@ -1,9 +1,7 @@
 package com.example.simplecrudapi.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
